a82649caf2d316ddc56ea2029014c8f4908395ab
RumiMotion <71284871+rumimotion@users.noreply.github.com>
2026-06-12 20:40:19 +0400

    Create page.tsx

---
+++ src/app/solo/page.tsx
@@ -0,0 +1,9 @@
+mkdir -p ~/Documents/DOMINO/dominoes/src/app/solo
+git add .
+git commit -m "feat: solo test mode"
+git push
+cd ~/Documents/DOMINO/dominoes
+mkdir -p src/app/solo
+cd ~/Documents/DOMINO/dominoes
+git add .
+git commit -m "feat: solo test mode"
